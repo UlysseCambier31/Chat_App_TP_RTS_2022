7daa889eb951d6797f0110a4d6b276fe6fd9cccf
PIQ-BDTech-ENSEA <71843648+PIQ-BDTech-ENSEA@users.noreply.github.com>
2022-12-14 13:19:20 +0100

    - Can now provide config.ini path as program argument.

--- src/rts/ensea/fr/ChatApplication.java
+++ src/rts/ensea/fr/ChatApplication.java
@@ -37,6 +37,8 @@ import java.util.List;
  * @see Application
  */
 public class ChatApplication extends Application {
+    static File configFile;
+
     /**
      * Run the JavaFX thread when the method start() is called.
      * Allow user to log, connect and send message.
@@ -44,10 +46,14 @@ public class ChatApplication extends Application {
      * @see ReceiveMessageThread
      */
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
-        File configFile = new File("config.ini");
-        ApplicationConfiguration appConfig = new ApplicationConfiguration(configFile); // Loading configuration of the application from config.ini file.
+        ApplicationConfiguration appConfig = null; // Loading configuration of the application from config.ini file.
+        try {
+            appConfig = new ApplicationConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         primaryStage.setTitle("Join Chat");
         GridPane grid = new GridPane();
@@ -74,93 +80,97 @@ public class ChatApplication extends Application {
 
         ChatClient client = null;
         try {
+            assert appConfig != null;
             client = new ChatClient(appConfig.getPort(), appConfig.getServerAddress());
         } catch(ArrayIndexOutOfBoundsException | SocketException e) {
             e.printStackTrace();
         }
         ChatClient finalClient = client;
-        joinbutton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                primaryStage.close();
-                ScrollPane scroll = new ScrollPane();
-                VBox conversation = new VBox(10);
-                scroll.setContent(conversation);
-                conversation.setAlignment(Pos.BOTTOM_LEFT);
-                String username = userTextField.getText();
-                ReceiveMessageThread receiveMessageHandlerThread = new ReceiveMessageThread(finalClient.getSocket(),username,conversation,scroll);
-                receiveMessageHandlerThread.start();
+        ApplicationConfiguration finalAppConfig = appConfig;
+        joinbutton.setOnAction(event -> {
+            primaryStage.close();
+            ScrollPane scroll = new ScrollPane();
+            VBox conversation = new VBox(10);
+            scroll.setContent(conversation);
+            conversation.setAlignment(Pos.BOTTOM_LEFT);
+            String username = userTextField.getText();
+            assert finalClient != null;
+            ReceiveMessageThread receiveMessageHandlerThread = new ReceiveMessageThread(finalClient.getSocket(),username,conversation,scroll);
+            receiveMessageHandlerThread.start();
+            try {
+                finalClient.connect(username);
+                //finalClient.send("Hi I'm "+username+" ! I've just arrived in the server !", username);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            joinbutton.setDisable(true);
+            GridPane secondGrid = new GridPane();
+            secondGrid.setAlignment(Pos.BOTTOM_CENTER);
+            secondGrid.setHgap(10);
+            secondGrid.setVgap(5);
+            secondGrid.setPadding(new Insets(25, 25, 25, 25));
+
+            TextField userInputField = new TextField();
+
+            Button sendButton = new Button();
+            sendButton.setText("➤");
+            sendButton.setOnAction(event1 -> {
+                String text = userInputField.getText();
                 try {
-                    finalClient.connect(username);
-                    //finalClient.send("Hi I'm "+username+" ! I've just arrived in the server !", username);
+                    finalClient.send(text, username);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                joinbutton.setDisable(true);
-                GridPane secondGrid = new GridPane();
-                secondGrid.setAlignment(Pos.BOTTOM_CENTER);
-                secondGrid.setHgap(10);
-                secondGrid.setVgap(5);
-                secondGrid.setPadding(new Insets(25, 25, 25, 25));
+                userInputField.setText("");
+            });
+            userInputField.setOnAction(event12 -> {
+                String text = userInputField.getText();
+                try {
+                    finalClient.send(text, username);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                userInputField.setText("");
+            });
 
-                TextField userInputField = new TextField();
+            HBox hbBtn2 = new HBox(10);
+            hbBtn2.setAlignment(Pos.CENTER);
+            hbBtn2.getChildren().add(sendButton);
 
-                Button sendButton = new Button();
-                sendButton.setText("➤");
-                sendButton.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        String text = userInputField.getText();
-                        try {
-                            finalClient.send(text, username);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        userInputField.setText("");
-                    }
-                });
-                userInputField.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        String text = userInputField.getText();
-                        try {
-                            finalClient.send(text, username);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        userInputField.setText("");
-                    }
-                });
+            GridPane gridUserInput = new GridPane();
+            gridUserInput.setAlignment(Pos.CENTER);
+            gridUserInput.add(userInputField,0,0);
+            gridUserInput.add(hbBtn2,1,0);
 
-                HBox hbBtn2 = new HBox(10);
-                hbBtn2.setAlignment(Pos.CENTER);
-                hbBtn2.getChildren().add(sendButton);
-
-                GridPane gridUserInput = new GridPane();
-                gridUserInput.setAlignment(Pos.CENTER);
-                gridUserInput.add(userInputField,0,0);
-                gridUserInput.add(hbBtn2,1,0);
-
-                secondGrid.add(scroll,0,0);
-                secondGrid.add(gridUserInput, 0, 1);
-                Scene secondScene = new Scene(secondGrid, appConfig.getChatWidth(), appConfig.getChatHeight());
+            secondGrid.add(scroll,0,0);
+            secondGrid.add(gridUserInput, 0, 1);
+            Scene secondScene = new Scene(secondGrid, finalAppConfig.getChatWidth(), finalAppConfig.getChatHeight());
 
 
-                // New window (Stage)
-                Stage newWindow = new Stage();
-                newWindow.setTitle("Chat Conversation");
-                newWindow.setScene(secondScene);
+            // New window (Stage)
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Chat Conversation");
+            newWindow.setScene(secondScene);
 
-                // Set position of second window, related to primary window.
-                newWindow.setX(primaryStage.getX()+100);
-                newWindow.setY(primaryStage.getY()+10);
+            // Set position of second window, related to primary window.
+            newWindow.setX(primaryStage.getX()+100);
+            newWindow.setY(primaryStage.getY()+10);
 
-                newWindow.show();
-            }
+            newWindow.show();
         });
 
         Scene scene = new Scene(grid, appConfig.getJoinWidth(), appConfig.getJoinHeight());
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        if (args.length>0){
+            configFile =  new File(args[0]);
+        } else {
+            System.out.println("wrn: No config.ini file path provided, using current directory config.ini.");
+            configFile =  new File("config.ini");
+        }
+        Application.launch();
     }
 }
